a613c238e106084bf4abb98d5eabded57d6990eb
VickyLouise <vickyclayton92@gmail.com>
2018-12-01 16:27:39 +0000

    adding validation and displaying results

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,21 @@
 class Idea < ApplicationRecord
+  validates :name, presence: true, length: 10..50
+
+  validates :foo, numericality: { only_integer: true }
+
   mount_uploader :picture, PictureUploader
 end
+
+class Foo < ApplicationRecord
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: true }
+
+  before_save :check_if_can_drink
+
+  def check_if_can_drink
+    self.can_drink = age >= 18
+  end
+end
+
+foo = Foo.new(name: 'A', age: 10)
+foo.save
--- db/migrate/20181201122421_create_ideas.rb
+++ db/migrate/20181201122421_create_ideas.rb
@@ -10,3 +10,19 @@ class CreateIdeas < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+# name,age
+# Adrian,29
+
+class CreateFoos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.integer :age
+      t.boolean :can_drink
+      t.timestamps
+    end
+  end
+end
+
+
